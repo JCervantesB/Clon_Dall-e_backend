f08bb2e4d88f84360216b3377a46007abf886b79
Julio Cervantes <imjcervantes@gmail.com>
2023-01-24 20:49:03 -0700

    deploy

--- index.js
+++ index.js
@@ -9,7 +9,23 @@ import dalleRoutes from './routes/dalleRoutes.js';
 dotenv.config();
 
 const app = express();
-app.use(cors());
+
+//Configurar CORS
+
+const whitelist = [process.env.FRONTEND_URL];
+
+const corsOptions = {
+    origin: function(origin, callback){
+        if(whitelist.includes(origin)) {
+            callback(null, true)
+        } else {
+            callback(new Error('Error de Cors'))
+        }
+    }
+};
+
+app.use(cors(corsOptions));
+
 app.use(express.json({ limit: '50mb' }));
 
 app.use('/api/v1/post', postRoutes);
